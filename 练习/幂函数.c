e339d793dfb47e3880692da8742dbe59879912e3
Ke <616550293@qq.com>
2019-08-22 14:36:53 +0800

    1

--- 练习/幂函数.c
+++ 练习/幂函数.c
@@ -2,16 +2,14 @@
 int power(int m, int n);
 int main(void)
 {
-	int i;
-	i = 3;
+	int i = 3;
 	printf("%d %d %d", i, power(2, i), power(-3, i));
 	return 0;
 }	
 int power(int m, int n)
 {
-	int i, p;
-	p = 1;
-	for (i = 0;i < n; ++i)
+	int p = 1;
+	for (p = 1; n > 0; n--)
 		p = p * m;
 	return p;
 }
